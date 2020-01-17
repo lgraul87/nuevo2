ec17a2cd6c4b8d53dbd80c9413514829899fca20
Raul_Lora_Gomez <lgraul87@gmail.com>
2020-01-17 09:52:25 +0100

    cambio1

---
+++ REPO_NEW/src/REPO_NEW.java
@@ -0,0 +1,9 @@
+
+public class REPO_NEW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Cambio numero 1");
+	}
+
+}
---
+++ TRABAJO/src/REPOSITORIO_GITHUB.java
@@ -0,0 +1,9 @@
+
+public class REPOSITORIO_GITHUB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cambio numero 1");
+	}
+
+}
